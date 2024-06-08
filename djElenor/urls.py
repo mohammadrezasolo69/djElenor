95fddfe4d347b40243613a08999ad312aa11f162
Mohammadreza <mohammdrezasolo69@gmail.com>
2024-06-08 12:01:09 +0330

    :beers: delete files from app account

--- djElenor/urls.py
+++ djElenor/urls.py
@@ -1,18 +1,23 @@
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import include, re_path, path
+from django.urls import include, path
 
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
-urlpatterns = [
-                  path('', include('djElenor.account.urls')),
-                  path('admin/', admin.site.urls),
-                  re_path(
-                      r"^graphql/$",
-                      csrf_exempt(GraphQLView.as_view(graphiql=True)),
-                      name="api",
-                  ),
+urlpatterns_apps = []
 
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("api/", csrf_exempt(GraphQLView.as_view(graphiql=True)), name="api"),
+
+    path('', include(urlpatterns_apps)),
+
+]
+
+# ------------------------------------------ Config Static ----------------------------------------------------------
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
